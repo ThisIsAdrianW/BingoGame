ea2fb674a33f742fd9601c6c0b2bb855c90ee4ac
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2018-10-12 00:11:08 +0200

    Early bulid for MVVM

---
+++ app/src/main/java/com/thisis/adrianw/bingogame/MainActivity.java
@@ -0,0 +1,27 @@
+package com.thisis.adrianw.bingogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.thisis.adrianw.bingogame.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
+        // Create new fragment and transaction
+        Fragment mapThree = new MapThree();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, mapThree);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
