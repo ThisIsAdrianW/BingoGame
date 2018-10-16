5db6d3c3499da35defe8519a52c37d3c300677c5
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2018-10-17 01:15:59 +0200

    Created basic DAO interface

---
+++ app/src/main/java/com/thisis/adrianw/bingogame/Bingodata/BingoDao.java
@@ -0,0 +1,33 @@
+package com.thisis.adrianw.bingogame.Bingodata;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+@Dao
+public interface BingoDao {
+    @Insert
+    void insertIndex(IndexWord indexWord);
+
+    @Insert
+    void inserWord(Words words);
+
+    @Insert
+    void insertWords(Words... words);
+
+    @Delete
+    void delete(IndexWord indexWord);
+
+    @Query("SELECT * FROM words WHERE indexforword=:indexWord ORDER BY RANDOM() LIMIT 1")
+    List<Words> getWordsList(final String indexWord);
+
+    @Query("SELECT count(*) from words WHERE indexforword=:numOfWords")
+    int numberOfWordsForIndex(final String numOfWords);
+
+    @Query("Select * from indexword ORDER BY indexforwords COLLATE UNICODE")
+    List<IndexWord> selectAllIndexWords();
+
+}
