44c5ee703a96c9fca3599ed55c0aba3bfcba6323
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2018-10-16 23:17:16 +0200

    Creating package for database

--- app/src/main/java/com/thisis/adrianw/bingogame/MainActivity.java
+++ app/src/main/java/com/thisis/adrianw/bingogame/MainActivity.java
@@ -5,10 +5,7 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-
-
 import com.thisis.adrianw.bingogame.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
