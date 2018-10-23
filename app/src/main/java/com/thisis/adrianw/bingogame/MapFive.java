0b8017b777f4a4367ae996ebcd51c977c7207f12
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2018-10-23 06:15:33 +0200

    Last update on night shift ^_^

--- app/src/main/java/com/thisis/adrianw/bingogame/MapFive.java
+++ app/src/main/java/com/thisis/adrianw/bingogame/MapFive.java
@@ -4,10 +4,16 @@ package com.thisis.adrianw.bingogame;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.thisis.adrianw.bingogame.databinding.FragmentMapFiveBinding;
+import com.thisis.adrianw.bingogame.databinding.FragmentMapThreeBinding;
 
 
 public class MapFive extends Fragment {
@@ -15,6 +21,7 @@ public class MapFive extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+    private GameViewModel model;
 
 
     public MapFive() {
@@ -37,13 +44,30 @@ public class MapFive extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_five, container, false);
+        final FragmentMapFiveBinding binding = FragmentMapFiveBinding.inflate(inflater, container, false);
+        binding.setLifecycleOwner(this);
+        model = ViewModelProviders.of(getActivity()).get(GameViewModel.class);
+        binding.setViewModel(model);
+        model.setCurrentBoardModel(24);
+        binding.setActivity(MapFive.this);
+        binding.setList(model.returnStringList());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        MenuItem clearItem= menu.findItem(R.id.clearBoard);
+        MenuItem changeItem = menu.findItem(R.id.changeBoard);
+        clearItem.setVisible(true);
+        changeItem.setVisible(true);
     }
 
 }
