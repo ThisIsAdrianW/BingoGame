1ef44ec7ff1663b39843064b8ce030c2f96a3636
ThisIsAdrianW <35401114+ThisIsAdrianW@users.noreply.github.com>
2018-10-25 17:24:00 +0200

    Deleted more unused logs and imports

--- app/src/main/java/com/thisis/adrianw/bingogame/MapFive.java
+++ app/src/main/java/com/thisis/adrianw/bingogame/MapFive.java
@@ -4,8 +4,6 @@ package com.thisis.adrianw.bingogame;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
